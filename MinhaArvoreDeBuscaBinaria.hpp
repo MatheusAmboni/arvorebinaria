90aaa7f50ad01602a7fc9ff99dc5b2d338b1471f
Matheus Amboni <mateussamboni@gmail.com>
2022-02-12 15:48:50 -0300

    segundo commit

---
+++ MinhaArvoreDeBuscaBinaria.hpp
@@ -0,0 +1,19 @@
+#ifndef MINHAARVOREDEBUSCABINARIA_HPP
+#define MINHAARVOREDEBUSCABINARIA_HPP
+
+#include "ArvoreDeBuscaBinaria.hpp"
+#include <cassert>
+#include <utility>
+
+/**
+ * @brief Representa uma árvore binária de busca.
+ * 
+ * @tparam T O tipo de dado guardado na árvore.
+ */
+template<typename T>
+class MinhaArvoreDeBuscaBinaria : public ArvoreDeBuscaBinaria<T>
+{
+    //implemente aqui
+}
+
+#endif
---
+++ MinhaListaEncadeada.hpp
@@ -0,0 +1,12 @@
+#ifndef MINHALISTAENCADEADA_H
+#define MINHALISTAENCADEADA_H
+
+#include "ListaEncadeadaAbstrata.hpp"
+
+template <typename T>
+class MinhaListaEncadeada :  public ListaEncadeadaAbstrata<T>
+{
+    //implemente aqui
+};
+
+#endif
